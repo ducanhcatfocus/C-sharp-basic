42b3e8445ce448878e5c5da2eb8424dc8f828348
nguyen duc anh <catandfocus@gmail.com>
2023-05-14 22:45:54 +0700

    update time

--- display_time/Program.cs
+++ display_time/Program.cs
@@ -4,7 +4,7 @@ using System;
 namespace SystemTime{
     class Program{
         static void Main(string[] args){
-            DateTime localDate = DateTime.Now;
+            string localDate = DateTime.Now.ToString("dd'-'MM'-'yyy HH:mm:ss");
             System.Console.WriteLine("Date time is: " + localDate);
         }
     }
