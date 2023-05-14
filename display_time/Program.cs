51b147a2d62c982d220110863a14fda962d6899c
nguyen duc anh <catandfocus@gmail.com>
2023-05-14 22:12:11 +0700

    date_time

---
+++ display_time/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace SystemTime{
+    class Program{
+        static void Main(string[] args){
+            DateTime localDate = DateTime.Now;
+            System.Console.WriteLine("Date time is: " + localDate);
+        }
+    }
+}
