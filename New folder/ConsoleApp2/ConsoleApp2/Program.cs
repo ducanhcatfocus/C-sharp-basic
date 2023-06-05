653442082dc08f97d8564239248bd155dc2ef05b
duc anh <catandfocus@gmail.com>
2023-06-05 21:10:58 +0700

    5/6/23

---
+++ New folder/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Nhap a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap b: ");
+            b= int.Parse(Console.ReadLine());
+            Console.WriteLine((float) a/b);
+            Console.ReadKey();  
+        }
+
+
+        
+    }
+}
---
+++ New folder/IndexOutOfRangeException/IndexOutOfRangeException/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexOutOfRangeException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<char> list = new List<char>();
+                list.InsertRange(0, new char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+                //int value = list[6];
+                int value1 = list[-1];
+
+
+
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.ToString());
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
